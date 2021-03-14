5bf68832930f85094817a050a6c3e6e48dca6dcc
jrmast87 <45945181+jrmast87@users.noreply.github.com>
2021-03-13 23:18:04 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,18 @@ function App() {
   const [isListening, setIsListening] = useState(false)
   const [note, setNote] = useState(null)
   const [savedNotes, setSavedNotes] = useState([])
+  const [vocabMap, setVocabMap] = useState([])
+
+  var passage = 'I like green eggs and ham'
+  var passageWords = passage.split(" ")
+  var count = Object.keys(passageWords).length
+  console.log(count)
+  for(let i = 1; i <= count; i++) {
+    console.log(passageWords[i]);
+  }
+
+  var vocabWords = []
+  //var vocabMap = vocabWords.map((vocabWord) => vocabWord)
 
   useEffect(() => {
     handleListen()
@@ -50,15 +62,41 @@ function App() {
 
   const handleSaveNote = () => {
     setSavedNotes([...savedNotes, note])
+    console.log('saved note ' + note + ' saved')
+    var speech = note.split(" ")
+    var count = Object.keys(speech).length
+    console.log(count)
+    for(let i = 0; i < count; i++) {
+      console.log(speech[i]);
+      console.log(passageWords[i]);
+      console.log(typeof(savedNotes))
+      if(speech[i] === passageWords[i]) {
+        console.log('Match!')
+      } else {
+        //vocabWords.push(passageWords[i])
+        vocabWords.push(passageWords[i])
+        console.log(vocabWords)
+      }
+    }
+
+    if(vocabWords.length < 1) {
+      vocabWords.push('No new Words!')
+      console.log(vocabWords)
+    } 
+    setVocabMap(vocabWords)
+    console.log('vocabmap ' + vocabMap)
     setNote('')
   }
+
+  
 
   return (
     <>
       <h1>Voice Notes</h1>
       <div className="container">
         <div className="box">
-          <h2>Current Note</h2>
+          <h2>Read the Passage!</h2>
+          <p>I like green eggs and Ham!</p>
           {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
           <button onClick={handleSaveNote} disabled={!note}>
             Save Note
@@ -69,10 +107,10 @@ function App() {
           <p>{note}</p>
         </div>
         <div className="box">
-          <h2>Notes</h2>
-          {savedNotes.map(n => (
-            <p key={n}>{n}</p>
-          ))}
+          <h2>New Practice Words</h2>
+            {vocabMap.map(n => (
+              <p key={n}>{n}</p>
+            ))}
         </div>
       </div>
     </>
